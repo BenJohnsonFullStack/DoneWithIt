6ee8938c9b64dac851a8137b630ae70f8b1e715e
Ben Johnson <monk.sound.design@gmail.com>
2023-10-10 18:54:14 -0400

    logo and text

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 
 export default function App() {
   return (
@@ -7,24 +7,54 @@ export default function App() {
       style={{
         backgroundColor: "#fff",
         flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
       }}
     >
       <View
         style={{
-          backgroundColor: "tomato",
-          width: "100%",
-          height: "8%",
+          flex: 0.5,
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      />
+      >
+        <Image
+          source={require("./assets/app-logo.png")}
+          style={{
+            width: 100,
+            height: 100,
+            marginTop: 50,
+          }}
+        />
+        <Text
+          style={{
+            color: "#111",
+            fontSize: 18,
+          }}
+        >
+          Sell What You Don't Need
+        </Text>
+      </View>
       <View
         style={{
-          backgroundColor: "turquoise",
-          width: "100%",
-          height: "8%",
+          flex: 1,
+          justifyContent: "flex-end",
+          alignItems: "center",
         }}
-      />
+      >
+        <View
+          style={{
+            backgroundColor: "tomato",
+            width: "100%",
+            height: 70,
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: "turquoise",
+            width: "100%",
+            height: 70,
+          }}
+        />
+      </View>
     </View>
   );
 }
