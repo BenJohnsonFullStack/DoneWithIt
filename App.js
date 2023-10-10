b99a2853db507567ef85c79fc21c92baff4bb102
Ben Johnson <monk.sound.design@gmail.com>
2023-10-10 15:23:41 -0400

    Components and Image on screen

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello React Native</Text>
+      <TouchableHighlight onPress={() => console.log("image tapped")}>
+        <Image
+          source={{ uri: "https://picsum.photos/200/300" }}
+          style={styles.image}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 26,
+  },
+  image: {
+    width: 200,
+    height: 300,
   },
 });
