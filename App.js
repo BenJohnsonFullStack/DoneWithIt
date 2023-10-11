00a4e7532da4bb73ebc6fc229876999cbbcc66f0
Ben Johnson <monk.sound.design@gmail.com>
2023-10-11 00:24:25 -0400

    welcome screen

--- App.js
+++ App.js
@@ -1,62 +1,9 @@
 // import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, ImageBackground, Image, Text } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <ImageBackground
-      source={require("./assets/app-hero-bg.webp")}
-      style={{
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Image
-          source={require("./assets/app-logo.png")}
-          style={{
-            width: 100,
-            height: 100,
-            marginTop: 100,
-          }}
-        />
-        <Text
-          style={{
-            color: "#111",
-            fontSize: 18,
-          }}
-        >
-          Sell What You Don't Need
-        </Text>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "flex-end",
-          alignItems: "center",
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "tomato",
-            width: "100%",
-            height: 70,
-          }}
-        />
-        <View
-          style={{
-            backgroundColor: "turquoise",
-            width: "100%",
-            height: 70,
-          }}
-        />
-      </View>
-    </ImageBackground>
-  );
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
